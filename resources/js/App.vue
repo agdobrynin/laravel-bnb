aaee9e97bf30e35f2167839d42e44a768073ff97
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-02-13 19:06:03 +0400

    dev: Fix default db credentials

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,7 +2,7 @@
 div
     nav(class="navbar bg-light border-bottom navbar-light")
         div(class="container-fluid")
-            router-link(class="navbar-brand me-auto" :to="{name: 'home'}") B&B
+            router-link(class="navbar-brand me-auto" :to="{name: 'home'}") Booking BnB
     div(class="container py-4 px-3 mx-auto")
         router-view
 </template>
