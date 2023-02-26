a9b704e406ea42bfc1d73ab365ba265104f63805
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-02-26 19:25:27 +0400

    feat: Move css classes for Transition to global css file.

--- resources/js/Components/BookableView/AvailabilityBooking.vue
+++ resources/js/Components/BookableView/AvailabilityBooking.vue
@@ -111,18 +111,4 @@ const check = async () => {
         left: 1.2rem
     }
 }
-
-.slide-fade-enter-active {
-    transition: all 0.3s ease-out;
-}
-
-.slide-fade-leave-active {
-    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-    transform: translateX(20px);
-    opacity: 0;
-}
 </style>
