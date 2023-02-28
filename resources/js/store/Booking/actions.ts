b17b6114f67521a6741fb3ea920cf964ef8cb17f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-02-28 16:38:36 +0400

    refactor: use destruct context in actions

--- resources/js/store/Booking/actions.ts
+++ resources/js/store/Booking/actions.ts
@@ -7,16 +7,16 @@ import { IBookingDates } from '@/Types/IBookingAvailability'
 const KEY_LAST_SEARCH_BOOKING_DATES = 'LastSearchBookingDates'
 
 const actions: ActionTree<IBookingState, any> = {
-    async saveLastSearchBookingDates(context: ActionContext<IBookingState, IBookingState>, payload: IBookingDates): Promise<void> {
+    async saveLastSearchBookingDates({ commit }: ActionContext<IBookingState, IBookingState>, payload: IBookingDates): Promise<void> {
         localStorage.setItem(KEY_LAST_SEARCH_BOOKING_DATES, JSON.stringify(payload))
-        context.commit('lastSearchBookingDates', payload)
+        commit('lastSearchBookingDates', payload)
     },
 
-    async restoreLastSearchBookingDates(context: ActionContext<IBookingState, IBookingState>): Promise<void> {
+    async restoreLastSearchBookingDates({ commit }: ActionContext<IBookingState, IBookingState>): Promise<void> {
         const data: string | null = localStorage.getItem(KEY_LAST_SEARCH_BOOKING_DATES)
 
         if (data) {
-            context.commit('lastSearchBookingDates', JSON.parse(data))
+            commit('lastSearchBookingDates', JSON.parse(data))
         }
     }
 }
