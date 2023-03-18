49f65cee2a1278b0114cdf683b35879c73ecaeff
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-03-18 10:22:48 +0400

    feat: Check booking review owner

--- app/Http/Controllers/Api/BookingByReviewController.php
+++ app/Http/Controllers/Api/BookingByReviewController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BookingByReviewKeyResource;
 use App\Models\Booking;
+use Illuminate\Http\Request;
 
 class BookingByReviewController extends Controller
 {
-    public function __invoke(string $reviewKey)
+    public function __invoke(string $reviewKey, Request $request)
     {
         if ($booking = Booking::findByReviewKey($reviewKey)) {
+            if ($booking->user && $request->user()?->id !== $booking->user_id) {
+                abort(403, 'Your are not owner this review');
+            }
+
             return  new BookingByReviewKeyResource($booking);
         }
 
