ebed8ffdf3b6e0ec94ad4ac2b2cbf224af9eb640
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-05-30 10:39:27 +0400

    dev: Add api doc for BookableReviewController.

--- app/Http/Controllers/Api/BookableReviewController.php
+++ app/Http/Controllers/Api/BookableReviewController.php
@@ -5,10 +5,34 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BookableReviewResource;
 use App\Models\Bookable;
+use App\Virtual\PaginateMeta;
+use App\Virtual\PaginateShort;
+use App\Virtual\Response\NotFoundErrorResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use OpenApi\Attributes as OA;
 
 class BookableReviewController extends Controller
 {
-    public function __invoke(Bookable $bookable)
+    #[OA\Get(
+        path: '/bookables/{bookable}/reviews',
+        summary: 'Get bookable list review with pagination',
+        tags: ['Review'],
+    )]
+    #[OA\PathParameter(ref: '#/components/parameters/bookable')]
+    #[OA\Response(
+        response: 200,
+        description: 'Success',
+        content: new OA\JsonContent(
+            type: 'object',
+            allOf: [
+                new OA\Schema(ref: BookableReviewResource::class),
+                new OA\Schema(ref: PaginateShort::class),
+                new OA\Schema(ref: PaginateMeta::class),
+            ],
+        ),
+    )]
+    #[NotFoundErrorResponse(description: 'Not found bookable by id')]
+    public function __invoke(Bookable $bookable): AnonymousResourceCollection
     {
         $perPage = env('PAGINATION_REVIEW_LIST_PER_PAGE', 5);
 
