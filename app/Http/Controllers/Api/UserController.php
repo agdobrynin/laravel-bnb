c51204584bd9d1cd21e532aa0c6d47a4257bc95a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-05-31 15:30:15 +0400

    dev: Add api doc for Virtual\Controllers\LaravelFortifyUser.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\FetchUserResource;
 use App\Virtual\Response\HeaderSetCookieToken;
 use App\Virtual\Response\HttpErrorResponse;
-use App\Virtual\Response\ValidationErrorResponse;
 use Illuminate\Http\Request;
 use OpenApi\Attributes as OA;
 
@@ -28,35 +27,5 @@ class UserController extends Controller
     public function __invoke(Request $request): FetchUserResource
     {
         return new FetchUserResource($request->user());
-    }
-
-    // Virtual methods for Laravel/Fortify
-    #[OA\Put(
-        path: '/user/profile-information',
-        summary: 'Update user profile information',
-        security: [['sanctum' => []]],
-        tags: ['User'],
-    )]
-    #[OA\RequestBody(
-        content: new OA\JsonContent(
-            required: ['first_name', 'last_name', 'email'],
-            properties: [
-                new OA\Property(property: 'first_name', title: 'user name', type: 'string', minimum: 2),
-                new OA\Property(property: 'last_name', title: 'user last name', type: 'string', minimum: 2),
-                new OA\Property(property: 'email', type: 'string', format: 'email')
-            ],
-        )
-    )]
-    #[ValidationErrorResponse]
-    #[HttpErrorResponse(code: 401, description: 'Unauthenticated')]
-    #[OA\Response(
-        response: 200,
-        description: 'Success update',
-        headers: [new HeaderSetCookieToken],
-        content: new OA\JsonContent(),
-    )]
-    public function profileInformation(): void
-    {
-        throw new \RuntimeException('Not implemented here.');
     }
 }
